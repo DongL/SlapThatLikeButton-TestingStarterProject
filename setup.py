a0056d22dce5255f2d89f0189761479f2b975fe6
Dong Liang <ldifer@gmail.com>
2022-09-02 23:55:50 -0700

    init

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+package_dir = \
+{'': 'src'}
+
+packages = \
+['slapping']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'slapping',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Dong Liang',
+    'author_email': 'ldifer@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,23 @@
-import pytest
 import sys
+
+import pytest
 
 
 @pytest.fixture
-def capture_stdout(monkeypatch):
+def capture_stdout(monkeypatch):  # depend on other fixture
     buffer = {"stdout": "", "write_calls": 0}
 
     def fake_write(s):
         buffer["stdout"] += s
         buffer["write_calls"] += 1
 
-    monkeypatch.setattr(sys.stdout, 'write', fake_write)
+    monkeypatch.setattr(sys.stdout, "write", fake_write)
     return buffer
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(
+    scope="session"
+)  # session: Only run once and cache the value and pass the same db_conn to all apps
 def db_conn():
     db = ...
     url = ...
